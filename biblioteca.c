6e00bed45781c9faf63f55e9c11e48da72cb94ce
Griffin38 <pedrolima.0000@gmail.com>
2014-05-25 13:38:03 +0100

    incrementar

--- biblioteca.c
+++ biblioteca.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "biblioteca.h"
+#include "readfile.h"
 
 
 int agregar(char *prefixo[], unsigned nivel, char *path){
@@ -11,6 +12,9 @@ int agregar(char *prefixo[], unsigned nivel, char *path){
 
 
 int incrementar(char *nome[], unsigned valor){
-	
-	return 0;
+  char *distrito = strdup(  nome[0]);	
+  char *concelho= strdup(  nome[1]);
+  char *freguesia = strdup(  nome[2]);
+  populaEstruturas(distrito,concelho,freguesia,valor,1);
+  return 0;
 }
