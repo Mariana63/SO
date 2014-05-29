3c2f67f89cf71e7dc7578373d4fae13204ddfc31
Griffin38 <pedrolima.0000@gmail.com>
2014-05-29 15:28:54 +0100

    Backup1
    o cliente ja comunica com o sserv

--- cliente.c
+++ cliente.c
@@ -1,16 +1,109 @@
 
 #include <fcntl.h>
-
-int main(int argc, char*argv[]){
-char buffer[100];
-
-int n= open("/tmp/Serv",O_WRONLY);
-
-
-read(0,buffer,100);
-write(n,buffer,sizeof(buffer));
+#include <string.h>
+int main(int argc, char*argv[]) {
+    char buffer[100];
+    memset(buffer, 0, 100);
+    int n = open("/tmp/Serv", O_WRONLY);
 
 
 
+    int escolha;
+    write(1, "1- Agregar\n2-Incrementar\n", 25);
+    read(0, buffer, 2);
 
+    if (buffer[0] == '1') {
+        char *p;
+        char nivel;
+        char prefixo[50];
+        char *ficheiro;
+
+
+        write(1, "Nivel 0, 1 ou 2:\n", 17);
+        read(0, buffer, 2);
+        if (buffer[0] <= '2' && buffer[0] >= '0') {
+            nivel = buffer[0];
+            write(1, "Digite o prefixo:\n Ex:Braga:Braga:Gualtar\n", 38);
+            
+           
+            
+            if(( read(0, buffer, 30))!=0){
+            p = strdup(buffer);
+
+            write(1, "Digite o nome do ficheiro:\n", 28);
+            read(0, buffer, 30);
+            
+            ficheiro = strdup(buffer);
+            //mandar para o pipe
+            write(n, "a:", sizeof (char)*2);
+            write(n, nivel, 1);
+            write(n, p, sizeof (p));
+            write(n, ':', 1);
+            write(n, ficheiro, sizeof(ficheiro));
+            } else{/*/erro*/}
+        }
+        else { /*/errro*/}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    } else if (buffer[0] == '2') {
+        char* p;
+        int nivel;
+        write(1, "0, 1 ou 2:\n", 12);
+        read(0, buffer, 1);
+        if (buffer[0] <= 2 && buffer[0] >= 0) {
+            nivel = atoi(buffer[0]);
+            
+            
+     /*   if (nivel >= 0) {
+            char distrito[30];
+            printf("Digite o Distrito:\n");
+            scanf("%s", distrito);
+            p[0] = distrito;
+
+            if (nivel >= 1) {
+                char concelho[30];
+                printf("Digite o Conselho:\n");
+                scanf("%s", concelho);
+                p[1] = concelho;
+
+                if (nivel == 2) {
+                    char freguesia[30];
+                    printf("Digite o Freguesia:\n");
+                    scanf("%s", freguesia);
+                    p[2] = freguesia;
+                }
+            }
+        }
+
+
+        int valor;
+        printf("Digite o valor a incrementar:\n");
+        scanf("%d", &valor);
+
+        //mandar para o pipe*/
+        }
+    }
+
+    close(n);
+
+
+    return (1);
 }
+
+
+
+
+
+
+
--- servidor.c
+++ servidor.c
@@ -1,19 +1,24 @@
 
 
-
-
-void servidor(){
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+void main(){
+    char buffer[100];
     char*dist[32];
     int pipe[32][2];
-    int n=0,r;
+    int n=0;
+    int r;
    mkfifo("/tmp/Serv",0666);
     int li = open("/tmp/Serv",O_RDONLY);
     while(1){
         
-        if((r=read(li,buffer,X))!=0){
+        if((r=read(li,buffer,2))!=0){
         
-        
-        
+            if (buffer[0]=='a'){
+write(1,"Areg1n",5);            
+                
+            }
         
         }
     
