24aed9d781611990610f798bfb8aad24588a1243
Inês Medeiros <63msim@gmail.com>
2014-06-01 13:51:44 +0100

    incrementar n funciona. hoje vou ter contigo.

--- cliente.c
+++ cliente.c
@@ -78,58 +78,50 @@ int main(int argc, char*argv[]) {
 
 
 
-
+    //INCREMENTAR int incrementar(char* nome[], unsigned valor)
     else if (buffer[0] == '2') {
         char* p[3];
-        int i = 1;
         char* ficheiro;
 
-        write(1, "Nível 0, 1 ou 2:\n", 17);
-        read(0, buffer, 2);
-        int nivel = atoi(&buffer[0]);
-        char nivel2 = buffer[0];
+        
+            write(1, "Digite o Distrito:\n", 19);
+            read(0, buffer, 30);
+            buffer[strlen(buffer)-1]='\0';
+            p[0]=strdup(buffer); memset(buffer, 0, 50);
 
-         if(nivel>=0){
-                write(1, "Digite o Distrito:\n", 19);
-                read(0, buffer, 30);
-                buffer[strlen(buffer)-1]='\0';
-                p[0]=strdup(buffer); memset(buffer, 0, 50);
-
-                if (nivel>=1){
-                    write(1, "Digite o Concelho:\n", 19);
-                    read(0, buffer, 30);
-                    buffer[strlen(buffer)-1]='\0';
-                    p[1]=strdup(buffer); memset(buffer, 0, 50);
+               
+            write(1, "Digite o Concelho:\n", 19);
+            read(0, buffer, 30);
+            buffer[strlen(buffer)-1]='\0';
+            p[1]=strdup(buffer); memset(buffer, 0, 50);
 
 
-                    if (nivel==2){
-                        write(1, "Digite a Freguesia:\n", 20);
-                        read(0, buffer, 30);
-                        buffer[strlen(buffer)-1]='\0';
-                        p[2]=strdup(buffer); memset(buffer, 0, 50);
-            }}}
-
-            else printf("ERRO NIVEL");
+                   
+            write(1, "Digite a Freguesia:\n", 20);
+            read(0, buffer, 30);
+            buffer[strlen(buffer)-1]='\0';
+            p[2]=strdup(buffer); memset(buffer, 0, 50);
 
 
+            write(1, "Digite o valor a incrementar:\n", 30);
+            read(0, buffer, 10);
+            int valor = atoi(&buffer[0]);
+            char valor2 = buffer[0];
+        
+           
             
             //mandar para o pipe -> n
+            //d:i:c:f:valor:9
             write(n,p[0],sizeof(char)*strlen(p[0]));
             write(n,":",1);
             write(n,"i:",2);
-            write(n,&nivel2,1);
+            write(n,p[1],sizeof(char)*strlen(p[0]));
             write(n,":",1);
-
-
-            
-            while (i <= nivel) {
-                write(n, p[i],sizeof(char)*strlen(p[i]));
-                write(n, ":", 1);
-                i++;
-            }
-    
+            write(n,p[2],sizeof(char)*strlen(p[0]));
+            write(n,":",1);
+            write(n,&valor2,1);
+            write(n,":",1);
             write(n, "9", 1);
-
     }
     
 
--- servidor.c
+++ servidor.c
@@ -19,10 +19,175 @@ int fAct(char*s) {
     }
 return f;
 }
+//d:a:nivel(:c:f):9
+void agregar(int g, int li, char* distrito){
+    int fl2 = 0;
+    char buffer[50];
+    int nivel=-1;
+        
+        write(pipek[g][1], "A", 1);
+        while ((read(li, buffer, 1)) != 0 && (!fl2)){
+
+            if (buffer[0]=='0' || buffer[0]=='1' || buffer[0]=='2'){
+                nivel = buffer[0];
+
+
+
+
+            }
+
+            
+
+
+            else if (buffer[0] == '9'){
+                write(pipek[g][1], "FIM", 3); fl2=1;
+            }
+
+            else write(pipek[g][1], (&buffer[0]), 1);
+        }
+}
+
+
+//li == :valor:c:f:9
+void incrementar(int g, int li, char* distrito){
+    int fl2 = 0, j=0;
+    char buffer[50], valor[100], conc[15], freg[15];
+    write(pipek[g][1], "I", 1);
+
+
+        while ((read(li, buffer, 1)) != 0 && (!fl2)){
+
+
+            while (buffer[0]>='0' || buffer[0]<='9'){
+                memset(valor, 0, 100);
+                read(li, buffer, 1);
+                valor[j] = buffer[0]; j++;
+            }
+            valor[j-1] = '\0';
+            write(pipek[g][1], valor, strlen(valor));
+
+
+            //tirar :
+            if(buffer[0]==':') read(li, buffer, 1);
+
+            while (buffer[0]!=':'){
+                j=0;
+                memset(conc, 0, 15);
+                read(li, buffer, 1);
+                conc[j] = buffer[0]; j++;
+            }
+            conc[j-1] = '\0';
+            write(pipek[g][1], conc, strlen(conc));
+
+
+            //tirar :
+            if(buffer[0]==':') read(li, buffer, 1);
+
+            while (buffer[0]!=':'){
+                j=0;
+                memset(freg, 0, 15);
+                read(li, buffer, 1);
+                freg[j] = buffer[0]; j++;
+            }
+            freg[j-1] = '\0';
+            write(pipek[g][1], freg, strlen(freg));
+
+            
+
+            if (buffer[0] == '9'){
+                write(pipek[g][1], "FIM", 3); fl2=1;
+            }
+
+            else write(pipek[g][1], (&buffer[0]), 1);
+        }
+}
+
+
+//ler do li e escrever para o g (pipek[g][1])
+//escrever em g: "d:c:f:valor"
+void existe(int g, int li, char* distrito){
+    int fl2 = 0;
+    char buffer[50];
+        
+        write(pipek[g][1], distrito, strlen(distrito));
+        write(pipek[g][1], ":", sizeof (char));
+        while ((read(li, buffer, 1)) != 0 && (!fl2)){
+
+            if (buffer[0]=='a') agregar(g, li, distrito);
+            
+            else if (buffer[0]=='i') incrementar(g, li, distrito);
+
+            else if (buffer[0] == '9'){
+                write(pipek[g][1], "FIM", 3); fl2=1;
+            }
+
+            else write(pipek[g][1], (&buffer[0]), 1);
+        }
+}
+
+
+
+void naoExiste(int g, int li, char* pref){
+    char buffer[15];
+    dist[n] = strdup(pref);
+    printf("distrito NOVO:%s\n",dist[n]);
+    pipe(pipek[n]);
+    n++;
+    
+
+    if (!fork()) {
+        char *Dist = strdup(pref);
+         
+        close(pipek[n-1][1]);
+        
+        while(1){
+        
+            if((read(pipek[n-1][0],buffer,1))!=0){
+                
+                 if ( buffer[0] == '9'){
+                 
+                 
+                 }
+                printf("--%c--\n",buffer[0]);
+                
+            }
+        
+        
+        
+        }
+        
+
+    } else {
+        close(pipek[n-1][0]);
+          
+       write(pipek[n-1][1], pref, strlen(pref));
+        write(pipek[n-1][1], ":", sizeof (char));
+        int fl = 0;
+        while ((read(li, buffer, 1)) != 0&& !fl) {
+
+
+            if (buffer[0]=='9') {  
+                printf("Acaboude mandar po filho novo:\n");
+                
+                write(pipek[n - 1][1], (&buffer[0]), 1);
+                
+                fl = 1;
+            } else {
+                write(pipek[n - 1][1], (&buffer[0]), 1);
+                
+            }
+        }//fork pa novo filho; add novo dist a lista e mandar pelo pipe
+
+    }
+
+}
+
+
+
 
 int main() {
     char buffer[50], pref[15];
-    int j = 0, i = 0, f = 0, r, nivel;
+    int j=0, i = 0, f = 0, r, nivel;
     n = 0;
 
    
@@ -31,155 +196,23 @@ int main() {
 
     while (1) {
         if ((r = read(li, buffer, 1)) != 0) {
-            j=0; 
-            memset(pref, 0, 15);
-            pref[j] = buffer[0];
-            j++;
+            j=0; memset(pref, 0, 15);
+            pref[j] = buffer[0]; j++;
          
                 while (buffer[0] != ':') {
                     read(li, buffer, 1);
                     pref[j] = buffer[0];
                     j++;
                 }
+
                 pref[j-1] = '\0';
                
                 int g = fAct(pref);
-                if (g != -1) {
-                    int fl2 = 0;
-                    write(pipek[g][1], pref, strlen(pref));
-                        write(pipek[g][1], ":", sizeof (char));
-                    while ((read(li, buffer, 1)) != 0 && (!fl2)) {
-                        if (buffer[0] == '9') {
-                            printf("Acabou de mandar para o que existe_:\n");
-                            write(pipek[g][1], (&buffer[0]), 1);
-                            fl2=1;
-                        } else {
-                            write(pipek[g][1], (&buffer[0]), 1);
-                           
-                        }
-
-                        //mandar para o filho pelo pipe g
-                    }
-                } else {
-                    dist[n] = strdup(pref);
-                    printf("distrito NOVO:%s\n",dist[n]);
-                    pipe(pipek[n]);
-                    n++;
-                    
-
-                    if (!fork()) {
-                        char *Dist = strdup(pref);
-                         
-                        close(pipek[n-1][1]);
-                        
-                        while(1){
-                        
-                            if((read(pipek[n-1][0],buffer,1))!=0){
-                                
-                                 if ( buffer[0] == '9'){
-                                 
-                                 
-                                 }
-                                printf("--%c--\n",buffer[0]);
-                                
-                            }
-                        
-                        
-                        
-                        }
-                        
-
-                    } else {
-                        close(pipek[n-1][0]);
-                          
-                       write(pipek[n-1][1], pref, strlen(pref));
-                        write(pipek[n-1][1], ":", sizeof (char));
-                        int fl = 0;
-                        while ((read(li, buffer, 1)) != 0&& !fl) {
-
-
-                            if (buffer[0]=='9') {  
-                                printf("Acaboude mandar po filho novo:\n");
-                                
-                                write(pipek[n - 1][1], (&buffer[0]), 1);
-                                
-                                fl = 1;
-                            } else {
-                                write(pipek[n - 1][1], (&buffer[0]), 1);
-                                
-                            }
-                        }//fork pa novo filho; add novo dist a lista e mandar pelo pipe
-
-                    }
-                }
-
-           
-
+                if (g != -1) existe(g, li, pref);
+                else naoExiste(g, li, pref);
         }
-
-
     }
 
     return (EXIT_SUCCESS);
 }
-    /* read(li,buffer,1);
-                 nivel = buffer[0];
-                 printf("nI:%d\n",nivel);
-                  memset(pref,0,15);
-                  while( i<=nivel){
-                 
-                     read(li,buffer,1);
-                      printf("I:-%c- \n",buffer[0]);
-                     if(buffer[0]==':')
-                     {printf("I,cenas.%s\n",pref);
-                     char *aux = strdup(pref);
-                         prefixo[i] = aux;
-                         memset(pref,0,15);
-                         i++;
-                         j=0;
-                     }else
-                     {
-                 
-                
-                     pref[j] = buffer[0];
-                     j++;
-                
-                     }
-                  printf("NivelI: %d,prefixo %s-buff-%c",i,prefixo[i],buffer[0]);  
-                 }*/
 
-    /*read(li,buffer,1);
-                 nivel = buffer[0]-'0';
-                 printf("nA:%d\n",nivel);
-            
-        
-                 memset(pref,0,15);
-               
-                  while( i<=nivel){
-                  
-                     read(li,buffer,1);
-                     printf("A:-%c-\n",buffer[0]);
-                     if(buffer[0]==':')
-                     {printf("A,cenas\n");
-                         prefixo[i] = strdup(pref);
-                         memset(pref,0,15);
-                         i++;
-                     }else
-                     {
-                 
-                
-                     pref[j] = buffer[0];
-                     j++;
-                
-                     }
-                 
-                 }
-                 j=0;
-                 while(buffer[0]!=':'){
-                 
-                     pref[j]= buffer[0];
-                }
-                 pref[strlen(pref)-1]='\0';
-               char *  ficheiro = strdup(pref);
-             printf("NiVA: %d, %s-%c",nivel,ficheiro,buffer[0]); 
-              // agregar(prefixo,nivel,ficheiro);*/
